657b8d9721386042be320e0eb0fe1410e324a87a
anthony villacis <anthony@anthonys-MacBook-Air.local>
2025-03-06 16:37:52 -0500

    cambios

--- addons/invoice_migration/models/account_move.py
+++ addons/invoice_migration/models/account_move.py
@@ -1,4 +1,6 @@
 from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError,UserError
+import json
 
 class AccountMove(models.Model):
     _inherit = "account.move"
@@ -8,6 +10,24 @@ class AccountMove(models.Model):
     parcial = fields.Boolean(string="Factura Parcial")
     datos_pagos_json = fields.Text(string="Pagos Json")
     total_antes = fields.Float(string='Importe total sistema antiguo')
+
+
+    def agregar_pagos(self):
+        for record in self:
+            if record.datos_pagos_json:
+                try:
+                    datos_pagos = json.loads(record.datos_pagos_json)
+                    for pago in datos_pagos.get('content', []):
+                        ref = pago.get('ref', '')
+                        amount = pago.get('amount', 0.0)
+                        if ref and amount:
+                            record.details_ids.create({
+                                'move_id': record.id,
+                                'name': ref,
+                                'amount': amount,
+                            })
+                except json.JSONDecodeError:
+                    raise UserError("El formato del JSON en 'Pagos Json' no es válido.")
 
 
 

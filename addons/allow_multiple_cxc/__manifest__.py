494f099a620462a68447dd542be4d39ffade5b8a
anthony villacis <anthony@anthonys-MacBook-Air.local>
2025-03-26 17:27:12 -0500

    cambios

---
+++ addons/allow_multiple_cxc/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'permitir multipls cuentas',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'base','account'
+    ],
+    # 'data': [
+    #     ''
+    # ],
+    # 'demo': [
+    #     ''
+    # ],
+    # 'auto_install': False,
+    # 'application': False,
+    # 'assets': {
+        
+    # }
+}

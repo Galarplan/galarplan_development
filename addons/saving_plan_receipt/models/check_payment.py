54d8a80209e4303f3c92ebdf6f824a0cbff47bdf
anthony villacis <anthony@anthonys-MacBook-Air.local>
2025-09-02 08:36:11 -0500

    cambios

--- addons/saving_plan_receipt/models/check_payment.py
+++ addons/saving_plan_receipt/models/check_payment.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError,ValidationError
+import base64
 
 class ReceiptValidation(models.Model):
     _name = 'receipt.validation'
@@ -179,6 +180,73 @@ class ReceiptValidation(models.Model):
         
         
         return base_options
+    
+    def print_receipt(self):
+        self.ensure_one()
+        
+        # try:
+        #     # Método seguro para Odoo 16
+        #     uid = self.env['res.users'].authenticate(
+        #         self.env.cr.dbname,
+        #         self.usuario,
+        #         self.password,
+        #         {}
+        #     )
+        #     if not uid:
+        #         raise ValidationError(_('Usuario o contraseña incorrectos'))
+                
+        #     user = self.env['res.users'].browse(uid)
+        # except Exception as e:
+        #     # raise ValidationError(_('Error de autenticación: %s') % str(e))
+        #     raise ValidationError('Error de autenticación vuelve a cerrar la venta e ingresa nuevamente tus credenciales')
+        
+        # Verificar estado del recibo
+        if self.state not in ('posted','verified'):
+            raise ValidationError('El recibo debe estar en estado "Publicado" para poder imprimirse')
+        
+        # Registrar el usuario que imprime
+        if not self.printed_by:
+                self.printed_by = self.env.user.id,
+                self.print_date = fields.Datetime.now()
+            
+        
+        val = True
+        if val:
+            # Obtener la referencia del reporte
+            report_ref = 'saving_plan_receipt.action_report_receipt_validation' 
+            # Obtener el objeto report
+            report = self.env['ir.actions.report']._get_report_from_name(report_ref)
+            
+            # Renderizar el reporte con todos los parámetros requeridos
+            html_content, _ = report._render_qweb_html(
+                report_ref=report_ref,
+                docids=[self.id],
+                data={}
+            )
+            
+            # Codificar el contenido HTML en base64
+            html_content_base64 = base64.b64encode(html_content)
+            
+            # Crear un adjunto temporal
+            attachment = self.env['ir.attachment'].create({
+                'name': f'Recibo_cobro_{self.name}.html',
+                'type': 'binary',
+                'datas': html_content_base64,
+                'res_model': self._name,
+                'res_id': self.id,
+                'mimetype': 'text/html'
+            })
+            
+            # Devolver acción para descargar el archivo
+            return {
+                'type': 'ir.actions.act_url',
+                'url': '/web/content/%s?download=true' % attachment.id,
+                'target': 'new',
+            }
+        else:
+        
+            # Retornar la acción del reporte
+            return self.env.ref('saving_plan_receipt.action_report_receipt_validation').report_action(self.receipt_id)
     
     def sum_inscription(self,lines):
         value = 0
